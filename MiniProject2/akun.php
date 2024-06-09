fc69c1a02315f916712b16ecf6d6e36279e6e79d
alanalzi <117884402+alanalzi@users.noreply.github.com>
2024-06-09 16:44:20 +0700

    penambahan button hapus dan alert

--- MiniProject2/akun.php
+++ MiniProject2/akun.php
@@ -18,8 +18,6 @@
 
     $hitungpanjangpassword = strlen($password);
     $penutuppassword = str_repeat('•', $hitungpanjangpassword);
-
-
 ?> 
 
 <!DOCTYPE html>
@@ -29,11 +27,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Akun Harmoni Konser</title>
     <link rel="stylesheet" href="akuncss.css">
-    <!-- <style>
-        .btn{
-            margin-bottom: 10px;
-        }
-    </style> -->
 </head>
 <body>
     <header>
@@ -61,11 +54,7 @@
                 </td>
                 <td>
                     <p class="tabelakun">
-                        <?php 
-                           echo $username;
-                        
-                        ?>
-                        
+                        <?php echo $username; ?>
                     </p>
                 </td>
             </tr>
@@ -77,11 +66,7 @@
                 </td>
                 <td>
                     <p class="tabelakun">
-                        <?php 
-                           echo $penutuppassword;
-                        
-                        ?>
-                        
+                        <?php echo $penutuppassword; ?>
                     </p>
                 </td>
             <tr>
@@ -92,11 +77,7 @@
                 </td>
                 <td>
                     <p class="tabelakun">
-                    <?php 
-                           echo $email;
-                        
-                        ?>
-                        
+                        <?php echo $email; ?>
                     </p>
                 </td>
             </tr>
@@ -134,18 +115,22 @@
                             Jumlah
                         </h4>
                     </td>
+                    <td>
+                        <h4>
+                            Aksi
+                        </h4>
+                    </td>
                 </tr>
             </thead>
-            <!-- !!!Jangan Di Hapus!!! -->
-            <div class="tabeltiketuserisi">
-                <?php $resultpemesanan = mysqli_query($conn, "SELECT * FROM pembelian WHERE id_user = '$user_id'");?>
-                <?php if(isset($resultpemesanan)) :?>
-                    <?php while($infotiket = mysqli_fetch_assoc($resultpemesanan)) : 
+            <tbody class="tabeltiketuserisi">
+                <?php
+                $resultpemesanan = mysqli_query($conn, "SELECT * FROM pembelian WHERE id_user = '$user_id'");
+                if(isset($resultpemesanan)) :
+                    while($infotiket = mysqli_fetch_assoc($resultpemesanan)) :
                         $id_pembelian = $infotiket['id_pembelian'];
                         $jumlahtiket = $infotiket['jumlah'];
                         $id_konser = $infotiket['id_konser'];                                                                    
                         $resultkonser = mysqli_query($conn, "SELECT * FROM konser_data WHERE id_konser = '$id_konser'");
-
                         if(mysqli_num_rows($resultkonser) === 1){
                             $infokonser = mysqli_fetch_assoc($resultkonser);
                             $judulkonser = $infokonser['judul'];
@@ -158,42 +143,55 @@
                                 $tanggalKonser = $tanggalKonserAwal. " - " .$tanggalKonserAkhir;
                             }
                         }
-                    ?>
+                ?>
                 <tr>
+                    <td><?php echo $judulkonser; ?></td>
+                    <td><?php echo $lokasikonser; ?></td>
+                    <td><?php echo $tanggalKonser; ?></td>
+                    <td><?php echo $jumlahtiket; ?></td>
                     <td>
-                        <?php
-                            if(isset($judulkonser)){
-                                echo $judulkonser;
-                            }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            if(isset($lokasikonser)){
-                                echo $lokasikonser;
-                            }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            if(isset($tanggalKonser)){
-                                echo $tanggalKonser;
-                            }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            if(isset($jumlahtiket)){
-                                echo $jumlahtiket;
-                            }
-                        ?>
+                        <form method="post" action="delete_ticket.php" onsubmit="return confirmDelete()">
+                            <input type="hidden" name="id_pembelian" value="<?php echo $id_pembelian; ?>">
+                            <button class="hapus" type="submit" name="delete">Hapus</button>
+                        </form>
                     </td>
                 </tr>
-                    <?php endwhile;?>
-                <?php endif;?>
-            </div>
-            <!-- !!!Jangan Di Hapus!!! -->
+                <?php endwhile; endif; ?>
+            </tbody>
         </table>
     </div>
+    <footer>
+        <div class="socialmedia">
+            <p>Follow Us!</p>
+            <div class="logososmed">
+                <table border="0">
+                    <tr>
+                        <td>
+                            <a href="https://www.facebook.com"><img src="logososmed/facebook.png" alt="fb"></a>
+                        </td>
+                        <td>
+                            <a href="https://www.instagram.com/"><img src="logososmed/instagram (2).png" alt="ig"></a>
+                        </td>
+                        <td>
+                            <a href="https://www.tiktok.com/id-ID/"><img src="logososmed/tiktok.png" alt="tiktok"></a>
+                        </td>
+                        <td>
+                            <a href="https://x.com/?lang=en"><img src="logososmed/twitter.png" alt="x"></a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <hr>
+        <div>
+            <p id="reg">&reg;Harmoni Musik Indonesia 2024</p>
+        </div>
+    </footer>
 </body>
 </html>
+<script>
+function confirmDelete() {
+    return confirm("Apakah Anda yakin untuk menghapus tiket ini?");
+}
+</script>
+
